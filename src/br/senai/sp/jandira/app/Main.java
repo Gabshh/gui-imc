4e9076690a28a19c7dc6a2e481909ac520c04b8f
GABRIEL GOMES SAVIO <21276337@CFP127ALUNO.LOCAL>
2021-10-21 16:50:09 -0300

    first commit

---
+++ src/br/senai/sp/jandira/app/Main.java
@@ -0,0 +1,41 @@
+package br.senai.sp.jandira.app;
+
+//import java.util.Scanner;
+
+import br.senai.sp.jandira.gui.Frame;
+//import br.senai.sp.jandira.status.TabelaStatus;
+//import br.senai.sp.jandira.valores.Matematica;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Frame Tela = new Frame();
+		Tela.criarTela();
+		
+		
+		
+		
+		
+	}
+//		// Adquirir dados do user (CONSOLE!!)
+//
+//		Scanner leitor = new Scanner(System.in);
+//		Matematica calculo = new Matematica();
+//		TabelaStatus status = new TabelaStatus();
+//
+//		System.out.print("Qual o seu peso (kg)? ");
+//		calculo.peso = leitor.nextInt();
+//
+//		System.out.print("Qual sua altura (?,??) ");
+//		calculo.altura = leitor.nextDouble();
+//
+//		leitor.close();
+//
+//		System.out.println("Seu IMC é");
+//		calculo.calcular();
+//
+//		
+//	}
+
+}
---
+++ src/br/senai/sp/jandira/gui/Frame.java
@@ -0,0 +1,134 @@
+package br.senai.sp.jandira.gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+import br.senai.sp.jandira.status.TabelaStatus;
+import br.senai.sp.jandira.valores.Matematica;
+
+public class Frame {
+	
+	public void criarTela() {
+		
+		JFrame meuFrame = new JFrame();
+		meuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		meuFrame.setSize(600, 600);
+		meuFrame.setTitle("IMC Cálculo");
+		meuFrame.setLayout(null);
+		
+		// Criar os componentes da tela
+		// X, Y, width, height
+		
+		JLabel lblPeso = new JLabel();
+		lblPeso.setText("Seu peso: ");
+		lblPeso.setBounds(20, 20, 80, 30);
+		
+		JTextField txtPeso = new JTextField();
+		txtPeso.setText(null);
+		txtPeso.setBounds(100, 20, 80, 30);
+		
+		JLabel lblAltura = new JLabel();
+		lblAltura.setText("Sua altura: ");
+		lblAltura.setBounds(20, 60, 80, 30);
+		
+		JTextField txtAltura = new JTextField();
+		txtAltura.setText("");
+		txtAltura.setBounds(100, 60, 80, 30);
+		
+		JButton btnCalcular = new JButton();
+		btnCalcular.setText("CALCULAR");
+		btnCalcular.setBounds(10, 100, 200, 30);
+		
+		// RESULTADOS
+		
+		JLabel lblResultados = new JLabel();
+		lblResultados.setText("Resultados:");
+		lblResultados.setBounds(20, 150, 150, 30);
+//		lblResultados.
+		
+		JLabel lblValor = new JLabel();
+		lblValor.setText("Valor IMC: ");
+		lblValor.setBounds(20, 200, 150, 30);
+		
+		JLabel lblValorResultado = new JLabel();
+		lblValorResultado.setText("");
+		lblValorResultado.setBounds(100, 200, 35, 30);
+		
+		JLabel lblEstado = new JLabel();
+		lblEstado.setText("Estado IMC: ");
+		lblEstado.setBounds(20, 230, 150, 30);
+		
+		JLabel lblEstadoResultado = new JLabel();
+		lblEstadoResultado.setText("");
+		lblEstadoResultado.setBounds(100, 230, 35, 30);
+		
+		
+		meuFrame.getContentPane().add(lblPeso);
+		meuFrame.getContentPane().add(txtPeso);
+		meuFrame.getContentPane().add(lblAltura);
+		meuFrame.getContentPane().add(txtAltura);
+		meuFrame.getContentPane().add(btnCalcular);
+		meuFrame.getContentPane().add(lblResultados);
+		meuFrame.getContentPane().add(lblValor);
+		meuFrame.getContentPane().add(lblEstado);
+		meuFrame.getContentPane().add(lblValorResultado);
+		meuFrame.getContentPane().add(lblEstadoResultado);
+		
+		
+		// POR ÚLTIMO!!
+		
+		meuFrame.setVisible(true);
+		
+		
+		
+		// EVENTOS DO BOTÃO
+		
+		btnCalcular.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				// Transformar strings em int
+				
+				String pesoString = txtPeso.getText();
+				String alturaString = txtAltura.getText();
+				
+				Matematica calculo = new Matematica();
+				
+				calculo.peso = Integer.parseInt(pesoString);
+				calculo.altura = Double.parseDouble(alturaString);
+				
+				calculo.calcular();
+				
+				lblValorResultado.setText("" + calculo.imc);
+				
+				// Mostrar Status
+				
+				TabelaStatus Estados = new TabelaStatus();
+				
+				Estados.verificarStatus();
+				
+				lblEstadoResultado.setText(calculo.status);
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+			}
+		});
+		
+		
+		
+	}
+
+}
